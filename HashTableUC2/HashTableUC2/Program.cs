af62e74d4a7946db4956e5782dd60ad5839f0c05
naveen962 <75928781+naveen962@users.noreply.github.com>
2021-10-07 04:16:53 -0700

    program to find the frequency of words in a large paragraph phase is added

---
+++ HashTableUC2/HashTableUC2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HashTableUC2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hash Table");
+            int key = 0;
+            string sampleParagraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] paragraph = sampleParagraph.Split(" ");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(paragraph.Length);
+            foreach (string word in paragraph)
+            {
+                hash.Add(key, word);
+                key++;
+            }
+            for (key = 0; key < paragraph.Length; key++)
+            {
+                string value = hash.Get(key);
+                Console.WriteLine($"frequency of word '{value}' is {key}");
+            }
+
+        }
+    }
+}
